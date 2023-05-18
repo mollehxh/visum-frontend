a55a744e889fee024da23654136048ebebf733bc
mollehxh <mollehxh@gmail.com>
2023-05-18 03:44:50 -0800

    feat: VIS-4 implement sign-in page

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -1,4 +1,5 @@
 import { SignUpPage } from '~/pages/sign-up';
+import { SignInPage } from '~/pages/sign-in';
 import { AppShell } from '~/shared/ui/app-shell';
 import { Header } from '~/shared/ui/header';
 import 'normalize.css';
@@ -7,7 +8,8 @@ export const Application = () => {
   return (
     <>
       <AppShell top={<Header />}>
-        <SignUpPage />
+        {/* <SignUpPage /> */}
+        <SignInPage />
       </AppShell>
     </>
   );
---
+++ src/pages/sign-in/sign-in-page.tsx
@@ -0,0 +1,60 @@
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import { Form, Input, Button, Card, Typography, Col, Row, Layout } from 'antd';
+
+export const SignInPage = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <Card
+        style={{ width: '90%', maxWidth: 400, padding: '12px 24' }}
+        bordered={false}
+      >
+        <Form name="sign-up" initialValues={{ remember: true }}>
+          <Typography.Title
+            level={3}
+            style={{ textAlign: 'center', margin: 0, marginBottom: 24 }}
+          >
+            Sign In
+          </Typography.Title>
+
+          <Form.Item
+            name="email"
+            rules={[
+              { required: true, message: 'Please input your email!' },
+              { type: 'email', message: 'Please enter a valid email!' },
+            ]}
+          >
+            <Input prefix={<MailOutlined />} placeholder="Email" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+              Sign In
+            </Button>
+          </Form.Item>
+
+          <Row justify="center">
+            <Col>
+              <Typography.Text>
+                Not registered? <Typography.Link>Sign Up</Typography.Link>
+              </Typography.Text>
+            </Col>
+          </Row>
+        </Form>
+      </Card>
+    </div>
+  );
+};
